cbed60a9a19c84855e9544082687e507c0499315
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-05-04 13:55:57 -0500

    weight changing on button click

--- src/components/StartWorkout/StartWorkoutItem.js
+++ src/components/StartWorkout/StartWorkoutItem.js
@@ -1,43 +1,71 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import IconButton from 'material-ui/IconButton';
-import Button from 'material-ui/Button';
 import { AddCircleOutline, RemoveCircleOutline } from 'material-ui-icons';
 
 import '../StartWorkout/StartWorkout.css'
 
 class StartWorkoutItem extends Component {
 
+    addFive = () => {
+        this.props.exerciseItem.default_weight = parseInt(this.props.exerciseItem.default_weight, 10)
+        this.props.exerciseItem.default_weight += 5;
+        console.log('weight', this.props.exerciseItem.default_weight);
+        return this.props.exerciseItem.default_weight;
+    }
+
+    deleteFive = () => {
+        console.log('clicked deleteFive');
+        this.props.exerciseItem.default_weight = parseInt(this.props.exerciseItem.default_weight, 10)
+        this.props.exerciseItem.default_weight -= 5;
+        console.log('weight', this.props.exerciseItem.default_weight);
+        return this.props.exerciseItem.default_weight;
+    }
+
+    addOneFor = (setsOrReps) => (event) => {
+        console.log('clicked addOneFor'); 
+    }
+    deleteOneFor = (setsOrReps) => (event) => {
+        console.log('clicked deleteOneFor');   
+    }
+
     render(){
 
-        let addButton = (
-            <IconButton size="small" variant="fab" color="primary" >
-                <AddCircleOutline />
-            </IconButton>
-        )
-
-        let subtractButton = (
-            <IconButton size="small" variant="fab" color="primary" >
-                <RemoveCircleOutline />
-            </IconButton>
-        )
-
         return (
+
         <ul key={this.props.exerciseItem.name}>
             <li><strong>Name: {this.props.exerciseItem.name} </strong> </li>
             <li>Sets: {this.props.exerciseItem.default_sets} 
-                    <div className="updateInWorkout"> 
-                        {addButton} 1 set {subtractButton}
-                    </div>
+                <div className="updateInWorkout"> 
+                    <IconButton onClick={this.addOneFor('sets')} size="small" color="primary" >
+                        <AddCircleOutline />
+                    </IconButton>
+                         1 set
+                    <IconButton onClick={this.deleteOneFor('sets')} size="small" color="primary" >
+                        <RemoveCircleOutline />
+                    </IconButton>
+                </div>
             </li>
             <li>Repetitions: {this.props.exerciseItem.default_reps} 
                 <div className="updateInWorkout"> 
-                    {addButton} 1 rep {subtractButton}
+                    <IconButton onClick={this.addOneFor('reps')} size="small" color="primary" >
+                        <AddCircleOutline />
+                    </IconButton>
+                    1 rep
+                    <IconButton onClick={this.deleteOneFor('reps')} size="small" color="primary" >
+                        <RemoveCircleOutline />
+                    </IconButton>
                 </div>                   
             </li>
             <li><strong>Resistance: {this.props.exerciseItem.default_weight} </strong>
                 <div className="updateInWorkout"> 
-                    {addButton} 5 lbs. {subtractButton}
+                    <IconButton onClick={this.addFive} size="small" variant="fab" color="primary" >
+                        <AddCircleOutline />
+                    </IconButton>
+                        5 lbs. 
+                    <IconButton onClick={this.deleteFive} size="small" variant="fab" color="primary" >
+                        <RemoveCircleOutline />
+                    </IconButton>
                 </div>  
             </li>
             <br/>
