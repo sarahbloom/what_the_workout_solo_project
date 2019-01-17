3019b632265f4219d656aec44f1e4165ad7bd2a0
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2019-01-16 20:34:10 -0600

    update npm packages

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.use('/api/exercise', exerciseRouter);
 app.use(express.static('build'));
 
 // App Set //
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
